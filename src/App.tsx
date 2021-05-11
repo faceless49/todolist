8e86db5e1bea98f5a68e8039288c6e9693866e65
Egor K <digital.zeus@mail.ru>
2021-05-11 23:12:31 +0300

    refactor todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import './App.css';
-import Todolist from "./components/todolist/Todolist";
+import Todolist, {TaskType} from './components/todolist/Todolist';
 
 
-function App(props: any) {
-
-  let tasks1 = [
-    {id: 1, title: 'CSS', isDone: true},
+function App() {
+  const tasks1 = [
+    {id: 1, title: 'HTML&CSS', isDone: true},
     {id: 2, title: 'JS', isDone: true},
-    {id: 3, title: 'React', isDone: false},
+    {id: 3, title: 'ReactJS', isDone: false},
   ]
+    // ? Подаем разные данные в две компоненты, для отрисовки другого JSX
+    // ? Для этого мы даем через пропсы массивы tasks
+    // ? И указываем их в Todolist в тайпе, что ждем массивы tasks
 
-  let tasks2 = [
-    {id: 1, title: 'Terminator', isDone: true},
-    {id: 2, title: 'XXX', isDone: false},
-    {id: 3, title: 'Fortune', isDone: true},
+  const tasks2: Array<TaskType> = [
+    {id: 1, title: 'HelloWorld', isDone: true},
+    {id: 2, title: 'I am Happy', isDone: false},
+    {id: 3, title: 'Yo', isDone: false},
   ]
 
   return (
     <div className="App">
       <Todolist title="What to learn" tasks={tasks1}/>
       <Todolist title="Songs" tasks={tasks2}/>
-      <Todolist title="React" tasks={tasks1}/>
     </div>
   );
 }
--- src/components/todolist/Todolist.tsx
+++ src/components/todolist/Todolist.tsx
@@ -1,25 +1,36 @@
-import Header from "./header/Header";
-import List from "./list/List";
-import Footer from "./footer/Footer";
-import React from "react";
+import React from 'react';
 
-type TaskType = {
-  id: number,
-  title: string,
-  isDone: boolean,
+
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
 }
 
-type PropsType = {
-  title: string,
-  tasks: Array<TaskType>,
+type PropsType = { // Мы создаем объект для передачи его в пропсы
+  title: string
+  tasks: Array<TaskType> // Массив чисел? Нет, Массив объектов тогда нужно описать type TaskType=
 }
+function Todolist(props: PropsType) { // props = {title: 'What to learn', tasks: []}
+debugger
 
-function Todolist(props: PropsType) {
   return (
     <div>
-      <Header title={props.tasks[0].title} />
-      <List/>
-      <Footer/>
+      <h3>{props.title}</h3>
+      <div>
+        <input/>
+        <button>+</button>
+      </div>
+      <ul>
+        <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+        <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+        <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
     </div>
   );
 }
