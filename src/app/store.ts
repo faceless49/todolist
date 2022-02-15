faeaa1ecd63e2bbcf11277037a08e9068dd38dfd
faceless49 <digital.zeus@mail.ru>
2022-02-15 17:32:53 +0300

    delete comments

--- src/app/store.ts
+++ src/app/store.ts
@@ -6,17 +6,13 @@ import { appReducer } from "./app-reducer";
 import { authReducer } from "../features/Login/auth-reducer";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
   tasks: tasksReducer,
   todoLists: todolistsReducer,
   app: appReducer,
   auth: authReducer,
 });
-// непосредственно создаём store
 export const store = createStore(rootReducer, applyMiddleware(thunk));
-// определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>;
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
 
