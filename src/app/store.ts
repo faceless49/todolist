bf0f08db790c3cbeaf6c5e15763aa4258aadbf33
faceless49 <digital.zeus@mail.ru>
2022-02-16 15:17:52 +0300

    install redux-toolkit. Configure store with middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,11 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import thunk, { ThunkAction } from "redux-thunk";
+import { combineReducers } from "redux";
+import thunk from "redux-thunk";
 import { tasksReducer } from "../features/TodolistsList/taskReducer";
 import { todolistsReducer } from "../features/TodolistsList/todolistsReducer";
 import { appReducer } from "./app-reducer";
 import { authReducer } from "../features/Login/auth-reducer";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
   tasks: tasksReducer,
@@ -12,7 +13,11 @@ const rootReducer = combineReducers({
   app: appReducer,
   auth: authReducer,
 });
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk),
+});
+
 export type AppRootStateType = ReturnType<typeof rootReducer>;
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
 
